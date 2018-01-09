ca85494c6c5f87c0539e25d0aa1f8768b08cb697
Herman-me <35011257+Herman-me@users.noreply.github.com>
2018-01-10 01:08:21 +0330

    adding style and theme
    added css files and bootstrap

--- index.php
+++ index.php
@@ -24,94 +24,36 @@
 </head>
 <body>
 	<div class="container">
-    <div class="row">
-      <div class="col-sm-12 header">
-        <img src="img/logo.png" alt="">
-        <img src="img/bird.png" alt="">
-      </div>
-    </div>
-    <div class="row">
+      <div class="row">
         <div class="col-sm-12 contain">
           <div class="row">
-            <div class="col-sm-12 menu">
-              <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                  <div class="navbar-header">
-                    <a class="navbar-brand" href="#">قالب سارینا</a>
-                  </div>
-                  <ul class="nav navbar-nav col-aps">
-                    <li class="active"><a href="#">خانه</a></li>
-                    <li><a href="#">آموزش های برنامه نویسی</a></li>
-                    <li><a href="#">آموزش های مهم</a></li>
-                    <li><a href="#">تماس با ما</a></li>
-                  </ul>
-                </div>
-              </nav>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-3 rightmenu">
-                  <div class="box">
-                    <h4>تیتر باکس</h4>
-                    <hr>
-                    <ul>
-                      <li><a href="#">آموزش های خوب</a></li>
-                      <li><a href="#">آموزش های خوب</a></li>
-                      <li><a href="#">آموزش های خوب</a></li>
-                      <li><a href="#">آموزش های خوب</a></li>
-                    </ul>
-                </div>
-                <div class="box">
-                  <h4>تیتر باکس</h4>
-                  <hr>
-                  <ul>
-                    <li><a href="#">آموزش های خوب</a></li>
-                    <li><a href="#">آموزش های خوب</a></li>
-                    <li><a href="#">آموزش های خوب</a></li>
-                    <li><a href="#">آموزش های خوب</a></li>
-                  </ul>
-              </div>
+            <div class="col-sm-6 ">
               <div class="box">
-                <h4>فرم ورود</h4>
-                <hr>
-                <form class="" action="index.html" method="post">
-                  <label>نام کاربری:</label><br>
-                  <input type="text" name="username"><br>
-                  <label>رمز عبور:</label><br>
-                  <input type="password" name="password"><br><br>
-                  <input type="submit" name="username" value="ورود ....">
-
+                <h1>آپلود سنتر لولو آپ</h1>
+                <form class="form" action="index.html" method="post">
+                  <label for="">فایل های خود را وارد کنید</label>
+                  <input type="file" name="" value="">
+                  <input type="file" name="" value="">
+                  <input type="file" name="" value="">
+                  <input type="submit" name="submit" value="آپلود کنید ..">
                 </form>
               </div>
             </div>
-            <div class="col-sm-9 leftmenu">
-              <div class="in-post">
-                <strong><h3>وب سایت ما و همه ی شما دوستان</h3></strong>
-                <div class="bio">
-                  <span class="fa fa-user"></span>  <a href=""> <span>نویسنده ی پست : مهدی رحیمی</span></a>
+            <div class="col-sm-6 ">
+              <div class="box">
+                <div class="intro">
+                  <h1>در مورد ما</h1>
+                  <p>سلام ما بر آن شدیم تا آپلود سنتری را راه اندازی کنیم برای خدمت رسانی به شما و همه ی دوستان و لطفا در توسعه ی آن به ما کمک کنید . و از شما میخواهیم برای مشکلات پیش آمده ما را همراهی کنید</p>
+                  <p>سلام ما بر آن شدیم تا آپلود سنتری را راه اندازی کنیم برای خدمت رسانی به شما و همه ی دوستان و لطفا در توسعه ی آن به ما کمک کنید . و از شما میخواهیم برای مشکلات پیش آمده ما را همراهی کنید</p>
+
                 </div>
-                <hr style="opacity:1;">
-                <img src="img/postpic.jfif" alt="">
-                <div class="body">
-                  <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p><a href="#">ادامه ...</a>
-              </div>
-              </div>
-              <div class="in-post">
-                <strong><h3>وب سایت ما و همه ی شما دوستان</h3></strong>
-                <div class="bio">
-                  <span class="fa fa-user"></span>  <a href=""> <span>نویسنده ی پست : مهدی رحیمی</span></a>
-                </div>
-                <hr style="opacity:1;">
-                <img src="img/postpic.jfif" alt="">
-                <div class="body">
-                  <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p><a href="#">ادامه ...</a>
-              </div>
               </div>
             </div>
+
           </div>
 
         </div>
-    </div>
+      </div>
   </div>
 </body>
 </html>
